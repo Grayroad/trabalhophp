6927f4c3e7f8f3e9febcdaa0fb6c35fc854673c1
Eduardo Soler <du.soler@hotmail.com>
2019-11-26 19:30:19 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+  $email = $_POST["email"];
+  $senha = $_POST["senha"];
+  if (empty($senha)) {
+    $mensagem = "Informe seu e-mail e senha!";
+  }
+}
+?>
+
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>App Help Desk</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <style>
+    .card-login {
+      padding: 30px 0 0 0;
+      width: 350px;
+      margin: 0 auto;
+    }
+  </style>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      App Help Desk
+    </a>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="card-login">
+        <div class="card">
+          <div class="card-header">
+            Login
+          </div>
+          <div class="card-body">
+            <form method="post">
+              <div class="form-group">
+                <input name="email" value="<?= $email ?>" type="email" class="form-control" placeholder="E-mail">
+              </div>
+              <div class="form-group">
+                <input name="senha" type="password" class="form-control" placeholder="Senha">
+              </div>
+
+              <?php if (!empty($mensagem)) { ?>
+
+                <div class="text-danger">
+                  <?= $mensagem ?>
+                </div>
+
+              <?php } ?>
+
+              <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
